ddd8ba1ee460df38d396a031472d794793599920
Charles Breton <charles.bretn@gmail.com>
2019-02-13 15:44:54 +0800

    space validations on name and location

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,6 @@
+# The Space class models pop-up spaces, identified by a name and location.
+# Spaces are rented by rentees via bookings
 class Space < ApplicationRecord
+  validates :name, presence: true
+  validates :location, presence: true, uniqueness: true
 end
