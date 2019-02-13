d721d21227b5e425963497d63690b19000bdb16d
Charles Breton <charles.bretn@gmail.com>
2019-02-13 16:51:02 +0800

    update space model: add validations for price and description

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,8 +1,10 @@
 # The Space class models pop-up spaces, identified by a name and location.
 # Spaces are rented by rentees via bookings
 class Space < ApplicationRecord
-  # validates :name, presence: true
-  # validates :location, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :location, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :description, presence: true
 
   # belongs_to :user
   # has_many :bookings
