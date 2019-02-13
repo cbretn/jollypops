e9e1396c69a36c280ae753c035904b29122536de
Charles Breton <charles.bretn@gmail.com>
2019-02-13 16:49:58 +0800

    add new attributes: price:integer, description:text

--- db/migrate/20190213084650_add_price_and_description_to_space.rb
+++ db/migrate/20190213084650_add_price_and_description_to_space.rb
@@ -1,4 +1,6 @@
 class AddPriceAndDescriptionToSpace < ActiveRecord::Migration[5.2]
   def change
+    add_column :spaces, :price, :integer
+    add_column :spaces, :description, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_074104) do
+ActiveRecord::Schema.define(version: 2019_02_13_084650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_13_074104) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
