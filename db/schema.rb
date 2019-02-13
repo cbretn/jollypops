d1a5b450179f9b1e4f903504a8d06d95af049dee
Charles Breton <charles.bretn@gmail.com>
2019-02-13 15:46:24 +0800

    run db:migration locally

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_055948) do
+ActiveRecord::Schema.define(version: 2019_02_13_074104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spaces", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
