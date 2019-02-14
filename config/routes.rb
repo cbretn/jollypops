9bfb331e7c371d34fc06dffc254cff39b714b2be
Charles Breton <charles.bretn@gmail.com>
2019-02-14 11:36:29 +0800

    drafted routes for spaces bookings messages and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :spaces
+  resources :spaces do
+
+    resources :bookings, only: [:index, :new, :create, :show] do
+      resources :messages, only: [:index, :create]
+    end
+
+    resources :reviews, only: [:index, :new, :create]
+  end
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
