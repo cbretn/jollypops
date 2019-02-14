761d083daa282a16391976f281f25dedf29ff28a
Charles Breton <charles.bretn@gmail.com>
2019-02-14 11:00:21 +0800

    wrote space policy

--- app/policies/space_policy.rb
+++ app/policies/space_policy.rb
@@ -4,4 +4,32 @@ class SpacePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
