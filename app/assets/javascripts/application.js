216ccf9fb32df7d50d323a348f2fcde1e10fc4be
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-02-18 22:09:51 +0800

    finished

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery
 //= require_tree .
 
-window.onload = function() {
+window.onclick = function() {
   const tl = new TimelineLite({ delay: 1 }),
     firstBg = document.querySelectorAll(".text__first-bg"),
     secBg = document.querySelectorAll(".text__second-bg"),
